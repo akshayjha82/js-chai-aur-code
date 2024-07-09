854f8d9ab0391cba1ff6be1ee64fbb503c5d5536
akshayjha82 <164542085+akshayjha82@users.noreply.github.com>
2024-07-09 15:40:38 +0000

    json config done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 1442
+let accountEmail= 'akshay82@gmail.com'
+var accountPassword = 'Akshay2407@'
+
+// accountId = 2
+accountEmail = "akshayjha92929"
+accountPassword = "14428"
+accountCity = "Jiapur"
+console.log(accountId);
+let accountState;
+console.table([accountEmail , accountId , accountPassword , accountCity, accountState])
+
+/*
+Don't use var since it has scope problem 
+*/
+
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("akshay")
+
+let name = "akshay"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* 
+1)numerical => 2 ka power 52
+2)bigInt => numerical se jyada range
+3)string ""
+4)boolean => true/false
+5)null
+6>undefined
+7)symbol=for uniqueness
+
+8)object
+*/
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
